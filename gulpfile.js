2af5fa9ff24ce83b9970e927e34d9b09a22d14f9
Richard Venneman <richardvenneman@me.com>
2015-08-29 13:35:35 +0200

    Configure CSS minification

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('default', function() {
   gulp.start('test');
@@ -20,11 +23,15 @@ gulp.task('test:watch', function() {
 gulp.task('css', function () {
   gulp.src('lib/scss/*.scss')
     .pipe(sass({
-      includePaths: require('node-bourbon').includePaths
+      includePaths: require('node-bourbon').includePaths,
+      outputStyle: 'expanded'
     }).on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('css:watch', function () {
-  gulp.watch('/lib/scss/*.scss', ['sass']);
+  gulp.watch('lib/scss/*.scss', ['sass']);
 });

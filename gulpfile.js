4724b8f9de7e5dc79dbab2849960fb02d6d317db
Richard Venneman <richardvenneman@me.com>
2015-08-29 14:00:07 +0200

    Configure CoffeeScript compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mocha = require('gulp-mocha');
 var sass = require('gulp-sass');
+var coffee = require('gulp-coffee');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
 
 gulp.task('default', function() {
   gulp.start('test');
@@ -16,10 +16,6 @@ gulp.task('test', function() {
     .on('error', gutil.log);
 });
 
-gulp.task('test:watch', function() {
-  gulp.watch(['lib/**', 'test/**'], ['mocha']);
-});
-
 gulp.task('css', function () {
   gulp.src('lib/scss/*.scss')
     .pipe(sass({
@@ -27,11 +23,16 @@ gulp.task('css', function () {
       outputStyle: 'expanded'
     }).on('error', sass.logError))
     .pipe(gulp.dest('dist/css'))
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(rename({ extname: '.min.css' }))
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('css:watch', function () {
-  gulp.watch('lib/scss/*.scss', ['sass']);
+gulp.task('coffee', function() {
+  gulp.src('lib/coffee/*.coffee')
+    .pipe(coffee({ bare: true }).on('error', gutil.log))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('dist/js'));
 });

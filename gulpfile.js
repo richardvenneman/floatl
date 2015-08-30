a4e7c26dba193536a7b0b9f48f28abe2659524a4
Richard Venneman <richardvenneman@me.com>
2015-08-30 10:33:34 +0200

    Remove tests, refactor compilation and distribution building tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
+// Gulp dependencies
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var buffer = require('gulp-buffer');
 
+// Style dependencies
 var sass = require('gulp-sass');
 
+// Build dependencies
+var buffer = require('gulp-buffer');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var coffeeify = require('coffeeify');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
-  gulp.start('build');
-});
+gulp.task('default', ['build']);
 
-gulp.task('css', function () {
-  gulp.src('lib/scss/*.scss')
-    .pipe(sass({
-      includePaths: require('node-bourbon').includePaths,
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('dist/css'));
-});
+//==============
+// Build tasks =
+//==============
 
-gulp.task('coffee', function() {
+// Build task: JavaScript compilation
+
+gulp.task('js', function() {
   var b = browserify({
     debug: true,
     entries: ['./lib/coffee/floatl.coffee'],
+    transform: [coffeeify]
+  });
+
+  return b.bundle()
+    .pipe(source('floatl.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('./build/js'))
+    .on('error', gutil.log);
+});
+
+gulp.task('js:dist', function() {
+  var b = browserify({
+    debug: false,
+    entries: ['./lib/coffee/floatl.coffee'],
+    standalone: 'Floatl',
     transform: [coffeeify]
   });
 
@@ -43,6 +53,37 @@ gulp.task('coffee', function() {
     .on('error', gutil.log);
 });
 
-gulp.task('build', ['css', 'coffee'], function() {
+// Build task: CSS compilation
 
+gulp.task('css', function() {
+  gulp.src('./lib/scss/*.scss')
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths,
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('css:dist', function() {
+  gulp.src('./lib/scss/*.scss')
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths,
+      outputStyle: 'expanded'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['js', 'css']);
+gulp.task('build:dist', ['js:dist', 'css:dist']);
+
+//===========
+// Watchers =
+//===========
+
+gulp.task('watch', function() {
+  gulp.watch(['lib/coffee/floatl.coffee'], ['js']);
+  gulp.watch(['lib/scss/floatl.scss'], ['css']);
 });

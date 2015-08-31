fe4c5a32a45a93dd55140d0e7bd29cc41d2b1b3b
Richard Venneman <richardvenneman@me.com>
2015-08-31 16:56:10 +0200

    Remove build step, add updated built files

--- gulpfile.js
+++ gulpfile.js
@@ -13,29 +13,10 @@ var browserify = require('browserify');
 var coffeeify = require('coffeeify');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['build']);
 
-//==============
-// Build tasks =
-//==============
-
-// Build task: JavaScript compilation
+// Tasks
 
 gulp.task('js', function() {
-  var b = browserify({
-    debug: true,
-    entries: ['./lib/coffee/floatl.coffee'],
-    transform: [coffeeify]
-  });
-
-  return b.bundle()
-    .pipe(source('floatl.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest('./build/js'))
-    .on('error', gutil.log);
-});
-
-gulp.task('js:dist', function() {
   var b = browserify({
     debug: false,
     entries: ['./lib/coffee/floatl.coffee'],
@@ -53,18 +34,7 @@ gulp.task('js:dist', function() {
     .on('error', gutil.log);
 });
 
-// Build task: CSS compilation
-
 gulp.task('css', function() {
-  gulp.src('./lib/scss/*.scss')
-    .pipe(sass({
-      includePaths: require('node-bourbon').includePaths,
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(gulp.dest('build/css'))
-});
-
-gulp.task('css:dist', function() {
   gulp.src('./lib/scss/*.scss')
     .pipe(sass({
       includePaths: require('node-bourbon').includePaths,
@@ -77,11 +47,8 @@ gulp.task('css:dist', function() {
 });
 
 gulp.task('build', ['js', 'css']);
-gulp.task('build:dist', ['js:dist', 'css:dist']);
 
-//===========
-// Watchers =
-//===========
+gulp.task('default', ['build']);
 
 gulp.task('watch', function() {
   gulp.watch(['lib/coffee/floatl.coffee'], ['js']);

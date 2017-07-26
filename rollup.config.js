c5af102a7937138cc1202a2722914e006047b932
Richard Venneman <richardvenneman@me.com>
2017-07-26 13:32:47 +0200

    Port over build process from Webpack to Rollup, remove Karma

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+
+export default [
+  {
+    entry: "src/js/index.js",
+    dest: pkg.browser,
+    format: "umd",
+    moduleName: "floatl",
+    plugins: [
+      babel({
+        exclude: ["node_modules/**"]
+      })
+    ]
+  },
+  {
+    entry: "src/js/index.js",
+    targets: [
+      { dest: pkg.main, format: "cjs" },
+      { dest: pkg.module, format: "es" }
+    ],
+    plugins: [
+      babel({
+        exclude: ["node_modules/**"]
+      })
+    ]
+  }
+];

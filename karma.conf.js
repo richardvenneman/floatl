9ed8a85342c6c99ed3eb736055518101baae8268
Richard Venneman <richardvenneman@me.com>
2018-02-09 02:58:20 +0700

    Configure SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,87 @@
 process.env.CHROME_BIN = require("puppeteer").executablePath();
 
+const sauceLabs = process.env.CI ? true : false;
+const sauceLabsLaunchers = {
+  sl_win_chrome: {
+    base: "SauceLabs",
+    browserName: "chrome",
+    platform: "Windows 10"
+  },
+  sl_mac_chrome: {
+    base: "SauceLabs",
+    browserName: "chrome",
+    platform: "macOS 10.12"
+  },
+  sl_firefox: {
+    base: "SauceLabs",
+    browserName: "firefox",
+    platform: "Windows 10"
+  },
+  sl_mac_firfox: {
+    base: "SauceLabs",
+    browserName: "firefox",
+    platform: "macOS 10.12"
+  },
+  sl_safari: {
+    base: "SauceLabs",
+    browserName: "safari",
+    platform: "macOS 10.12"
+  },
+  sl_edge: {
+    base: "SauceLabs",
+    browserName: "MicrosoftEdge",
+    platform: "Windows 10"
+  },
+  sl_ie_11: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    version: "11.103",
+    platform: "Windows 10"
+  },
+  sl_ie_10: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    version: "10.0",
+    platform: "Windows 7"
+  },
+  sl_ie_9: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    version: "9.0",
+    platform: "Windows 7"
+  },
+  sl_ie_8: {
+    base: "SauceLabs",
+    browserName: "internet explorer",
+    version: "8.0",
+    platform: "Windows 7"
+  },
+  sl_ios_safari_9: {
+    base: "SauceLabs",
+    browserName: "iphone",
+    version: "10.3"
+  },
+  "SL_ANDROID4.4": {
+    base: "SauceLabs",
+    browserName: "android",
+    platform: "Linux",
+    version: "4.4"
+  },
+  SL_ANDROID5: {
+    base: "SauceLabs",
+    browserName: "android",
+    platform: "Linux",
+    version: "5.1"
+  },
+  SL_ANDROID6: {
+    base: "SauceLabs",
+    browserName: "Chrome",
+    platform: "Android",
+    version: "6.0",
+    device: "Android Emulator"
+  }
+};
+
 module.exports = function(config) {
   config.set({
     frameworks: ["jasmine", "karma-typescript"],
@@ -11,9 +93,13 @@ module.exports = function(config) {
       "spec/**/*.ts": "karma-typescript"
     },
 
-    reporters: ["progress", "coverage", "karma-typescript", "notify"],
+    reporters: ["progress", "coverage", "karma-typescript", "notify"].concat(
+      sauceLabs ? "saucelabs" : []
+    ),
 
-    browsers: ["ChromeHeadless"],
+    customLaunchers: sauceLabs ? sauceLabsLaunchers : {},
+
+    browsers: sauceLabs ? Object.keys(sauceLabsLaunchers) : ["ChromeHeadless"],
 
     coverageReporter: {
       type: "lcov",

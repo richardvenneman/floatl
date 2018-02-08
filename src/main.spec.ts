c7359d49ee59c349d0913a4b2e83ab5dfffb4217
Richard Venneman <richardvenneman@me.com>
2018-02-08 17:52:02 +0700

    Implement main Floatl module

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,7 +1,122 @@
-import { hello } from './main'
+import { Floatl } from "./main";
 
-describe('Main', () => {
-  it("should work", () => {
-    expect(hello()).toEqual('Hello, world')
-  })
-})
+describe("Floatl", () => {
+  let fixture;
+
+  beforeEach(() => {
+    document.body.insertAdjacentHTML(
+      "afterbegin",
+      `<div id="fixture">
+      <div id="floatlOne" class="floatl">
+        <label class="floatl__label">Name</label>
+        <input id="floatlOneInput" type="text" class="floatl__input"></input>
+      </div>
+      <div id="floatlTwo" class="floatl">
+        <label class="floatl__label">Name</label>
+        <textarea id="floatlTwoInput" class="floatl__input"></textarea>
+      </div>
+    </div>`
+    );
+    fixture = document.getElementById("fixture");
+  });
+
+  afterEach(() => {
+    document.body.removeChild(fixture);
+  });
+
+  describe("Initialization", () => {
+    it("Inits with a HTML element", () => {
+      const action = () => {
+        return new Floatl(document.getElementById("floatlOne"));
+      };
+
+      expect(action).not.toThrow();
+    });
+
+    it(`adds ${Floatl.activeClass} class if input has a value`, () => {
+      const element = document.getElementById("floatlOne");
+      const input = document.getElementById("floatlOneInput");
+      (input as HTMLInputElement).value = "Initial value";
+      const floatl = new Floatl(element);
+
+      expect(element.classList.contains(`${Floatl.activeClass}`)).toBeTruthy();
+    });
+
+    it(`does not add ${Floatl.multilineClass} class if not a textarea`, () => {
+      const element = document.getElementById("floatlOne");
+      const floatl = new Floatl(element);
+
+      expect(
+        element.classList.contains(`${Floatl.multilineClass}`)
+      ).toBeFalsy();
+    });
+
+    it(`adds ${Floatl.multilineClass} class if applied to a textarea`, () => {
+      const element = document.getElementById("floatlTwo");
+      const floatl = new Floatl(element);
+
+      expect(
+        element.classList.contains(`${Floatl.multilineClass}`)
+      ).toBeTruthy();
+    });
+  });
+
+  describe("Focused state", () => {
+    let element;
+    let input;
+
+    beforeEach(() => {
+      element = document.getElementById("floatlOne");
+      input = document.getElementById("floatlOneInput");
+      const floatl = new Floatl(element);
+    });
+
+    it(`adds ${Floatl.focusedClass} class on focus`, () => {
+      expect(element.classList.contains(Floatl.focusedClass)).toBeFalsy();
+
+      input.focus();
+
+      expect(element.classList.contains(Floatl.focusedClass)).toBeTruthy();
+    });
+
+    it(`removes ${Floatl.focusedClass} class on blur`, () => {
+      input.focus();
+
+      expect(element.classList.contains(Floatl.focusedClass)).toBeTruthy();
+
+      input.blur();
+
+      expect(element.classList.contains(Floatl.focusedClass)).toBeFalsy();
+    });
+  });
+
+  describe("Active state", () => {
+    let element;
+    let input;
+
+    beforeEach(() => {
+      element = document.getElementById("floatlOne");
+      input = document.getElementById("floatlOneInput");
+      const floatl = new Floatl(element);
+    });
+
+    it(`adds ${Floatl.activeClass} class when entering characters`, () => {
+      expect(element.classList.contains(Floatl.activeClass)).toBeFalsy();
+
+      input.focus();
+      input.value = "test";
+      input.blur();
+
+      expect(element.classList.contains(Floatl.activeClass)).toBeTruthy();
+    });
+
+    it(`removes ${Floatl.activeClass} removing all characters`, () => {
+      input.value = "test";
+      input.blur();
+      input.value = "";
+      input.blur();
+
+      expect(element.classList.contains(Floatl.activeClass)).toBeFalsy();
+    });
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,51 @@
-export const hello = () => "Hello, world";
+import { addClass, addEventListener, removeClass } from "./utils";
+
+export class Floatl {
+  public static readonly focusedClass = "floatl--focused";
+  public static readonly activeClass = "floatl--active";
+  public static readonly multilineClass = "floatl--multiline";
+
+  private element: HTMLElement;
+  private label: Element;
+  private input: Element;
+
+  constructor(element: HTMLElement) {
+    this.element = element;
+    this.label = element.querySelectorAll(".floatl__label")[0];
+    this.input = element.querySelectorAll(".floatl__input")[0];
+
+    // Return early if not both the label and input are present
+    if (!this.label || !this.input) {
+      return;
+    }
+
+    if (this.input.nodeName === "TEXTAREA") {
+      addClass(this.element, Floatl.multilineClass);
+    }
+
+    this.handleChange();
+    this.bindListeners();
+  }
+
+  private handleChange() {
+    if ((this.input as HTMLInputElement | HTMLTextAreaElement).value === "") {
+      removeClass(this.element, Floatl.activeClass);
+    } else {
+      addClass(this.element, Floatl.activeClass);
+    }
+  }
+
+  private bindListeners() {
+    addEventListener(this.input, "focus", () => {
+      addClass(this.element, Floatl.focusedClass);
+    });
+
+    addEventListener(this.input, "blur", () => {
+      removeClass(this.element, Floatl.focusedClass);
+    });
+
+    for (const event of ["keyup", "blur", "change", "input"]) {
+      addEventListener(this.input, event, () => this.handleChange());
+    }
+  }
+}
